4fff6a0331c915c2f352715c22625e301b02efd0
Nirmal2000 <nirmalvelu2000@gmail.com>
2024-09-23 00:09:24 +0530

    Hardcode

--- src/app/page.js
+++ src/app/page.js
@@ -48,7 +48,8 @@ export default function Home() {
         setLoading(true);
 
         const pollStatus = async () => {          
-          const res = await fetch(process.env.PYTHON_API_CHECK_STATUS, {
+          // const res = await fetch(process.env.PYTHON_API_CHECK_STATUS, {
+          const res = await fetch('https://gos-backend.onrender.com/api/process_data', {
             method: "GET",
             headers: {
               "Authorization": `Bearer ${token}`, // Send token in headers
